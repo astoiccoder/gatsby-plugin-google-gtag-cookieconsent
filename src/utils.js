5f5da984b02197c878e12122427f012e7abccbf1
Uli <uli.astoiccoder@gmail.com>
2024-08-09 07:30:37 +0200

    feat: add simple configruation flag

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
 export const isPluginDisabled = (pluginOptions) => {
+    if (pluginOptions.enabled !== undefined) {
+        return !pluginOptions.enabled;
+    }
+    // keep for backwards compatibility
     return !pluginOptions.enableForAllEnvironments && process.env.NODE_ENV !== `production` && process.env.NODE_ENV !== `test`
 }

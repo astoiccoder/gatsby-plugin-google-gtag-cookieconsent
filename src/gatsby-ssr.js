205cc6cfcf55771998031be94a34155f06920f6f
Erik Hector <159142466+erik-hector@users.noreply.github.com>
2024-08-30 14:43:52 +0200

    Update gatsby-ssr.js

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -62,7 +62,13 @@ exports.onRenderBody = (
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-
+        
+        gtag('consent', 'default', {
+          'ad_storage': 'denied',
+          'ad_user_data': 'denied',
+          'ad_personalization': 'denied',
+          'analytics_storage': 'denied'
+        });
         ${pluginOptions.googleGtagPluginConfig.trackingIds
           .map(
             (trackingId) =>
